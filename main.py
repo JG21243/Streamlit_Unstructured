9d2d30e69d13d73a94eeaee64523c7ee31b2f595
Josh Glen <129418264+JG21243@users.noreply.github.com>
2023-09-05 19:53:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+import logging
 import openai
 from annoy import AnnoyIndex
 import streamlit as st
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from langchain.document_loaders import UnstructuredAPIFileLoader  # Import the UnstructuredAPIFileLoader
+from langchain.document_loaders import UnstructuredAPIFileLoader
 
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize OpenAI API key
 openai.api_key = st.secrets.openai_key
 
+# Initialize text storage
 text_storage = {}
 
+# Error handling function
 def handle_error(error_message):
+    logging.error(f"An error occurred: {error_message}")
     st.error(f"An error occurred: {error_message}")
     st.warning("Please try again.")
 
+# Function to extract text from PDF
 def extract_text_from_pdf(file, pages_per_chunk=8):
     try:
         loader = UnstructuredAPIFileLoader(
@@ -27,17 +34,13 @@ def extract_text_from_pdf(file, pages_per_chunk=8):
     except Exception as e:
         handle_error(f"Error opening PDF file: {e}")
         return []
-# Function to setup Annoy index
+
+# Function to set up Annoy index
 def setup_annoy(dimension=1536):
     index = AnnoyIndex(dimension, 'angular')
     return index
 
-# Initialize 'annoy_index' and 'index_built' if they're not already in session state
-if 'annoy_index' not in st.session_state:
-    st.session_state.annoy_index = setup_annoy()
-    st.session_state.index_built = False  # Add this line to track if the index has been built
-
-# Function to create an OpenAI embedding
+# Function to create OpenAI embedding
 def create_openai_embedding(text, max_words=700):
     if not text:
         handle_error("Input text is empty or None.")
@@ -107,15 +110,9 @@ def generate_answer(context_data, user_question):
         handle_error(f"Error generating answer: {e}")
         return None
 
-# Function to interpret an answer
-def interpret_answer(answer):
-    return answer
-
-# Initialize chat history
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
-
+# Updated main() function with chat history and user input logic
 def main():
+    logging.info("Starting the Streamlit application")
     st.title("PDF Question Answering")
     st.markdown("**Use AI to find answers to your questions from PDF documents.**")
     
@@ -123,8 +120,11 @@ def main():
     if 'annoy_index' not in st.session_state:
         st.session_state.annoy_index = setup_annoy()
         st.session_state.index_built = False  # Initialize index_built here
-    
-    
+
+    # Initialize chat history if not in session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
     # Sidebar for file upload
     with st.sidebar:
         st.subheader("Controls")
@@ -133,17 +133,21 @@ def main():
             submit_button = st.form_submit_button("Submit")
     
     if file is not None:
+        logging.info("File uploaded. Extracting text from PDF.")
         with st.spinner('Extracting text from PDF...'):
             st.session_state.text_chunks = extract_text_from_pdf(file)
         
         # Creating embeddings and upserting to Annoy
         for i, text in enumerate(st.session_state.text_chunks):
             if text:
+                logging.info(f"Creating OpenAI embedding for chunk {i}")
                 embedding = create_openai_embedding(text)
                 if embedding is not None:
+                    logging.info(f"Upserting to Annoy index for chunk {i}")
                     upsert_to_annoy(st.session_state.annoy_index, i, embedding, text)
         
         if not st.session_state.index_built:  # Check if the index has already been built
+            logging.info("Building the Annoy index.")
             st.session_state.annoy_index.build(20)
             st.session_state.index_built = True  # Mark the index as built
     
@@ -159,21 +163,19 @@ def main():
         user_input = st.text_input("Type your question here...")
         submit_button = st.form_submit_button("Submit")
     
-        interpreted_answer = None  # Initialize variable
-    
         if submit_button:
+            logging.info("User submitted a question.")
             st.session_state.chat_history.append({"role": "user", "content": user_input})
 
+            logging.info("Creating OpenAI embedding for the question.")
             question_embedding = create_openai_embedding(user_input)
             if question_embedding is not None:
+                logging.info("Querying Annoy index.")
                 query_results = query_annoy(st.session_state.annoy_index, question_embedding)
                 context_data = process_query_results(query_results)
+
+                logging.info("Generating an answer using OpenAI.")
                 answer = generate_answer(context_data, user_input)
-                interpreted_answer = interpret_answer(answer)
-
-            # Append the full answer to the chat history
-            st.session_state.chat_history.append({"role": "assistant", "content": interpreted_answer})
-
-if __name__ == "__main__":
-    main()
-
+                
+                # Append the full answer to the chat history
+                st.session_state.chat_history.append({"role": "assistant", "content": answer})
